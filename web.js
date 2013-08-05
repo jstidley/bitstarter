4ee5ea56391c4403260bd29ef3c98f37a8565ace
Ubuntu <ubuntu@ip-172-31-42-248.us-west-2.compute.internal>
2013-08-05 23:08:23 +0000

    homework 7

--- web.js
+++ web.js
@@ -7,9 +7,10 @@ app.get('/', function(request, response) {
 var file_buf = fs.readFileSync('./index.html');
 response.send(file_buf.toString('utf8'));
 
+
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
